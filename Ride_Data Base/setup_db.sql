fb4fc9f31152a7de84f982ffd82ec027fbf2b3dc
Jonathan2055 <j.mugisha3@gmail.com>
2025-10-16 12:04:00 +0200

    created a database and added a csv

--- Ride_Data Base/setup_db.sql
+++
@@ -1,7 +0,0 @@
--- Initialize Database and User for Urban Transport Analytics
-CREATE DATABASE IF NOT EXISTS `urban_transport` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-
-CREATE USER IF NOT EXISTS 'urban_user'@'%' IDENTIFIED BY 'urban_pass';
-GRANT ALL PRIVILEGES ON `urban_transport`.* TO 'urban_user'@'%';
-
-FLUSH PRIVILEGES;
